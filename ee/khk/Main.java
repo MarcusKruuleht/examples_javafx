f2bd56909f805f12f92bf87929cd22fe989aab16
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-18 19:29:27 +0300

    Scene klass

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,21 +1,28 @@
 package ee.khk;
 
 import javafx.application.Application;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
+import javafx.scene.Scene;
+import javafx.scene.Group;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
+
         Application.launch(args);
     }
 
-
     @Override
     public void start(Stage stage) {
+
+        Group root = new Group();
+        Scene scene = new Scene(root ,400, 150, Color.BLUE);
+        //scene.setFill(Color.BLUE); tausta värvi määramise alternatiiv
+        stage.setScene(scene);
+
         stage.setTitle("Hello javaFX");
-        stage.setWidth(250);
-        stage.setHeight(150);
+
         stage.show();
     }
 }

4fec0bd3b9ccce957c36df1eac2fe27ce31afefc
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-18 20:54:26 +0300

    FlowPane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -2,6 +2,7 @@ package ee.khk;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,7 +10,6 @@ import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
-import java.util.concurrent.Flow;
 
 
 public class Main extends Application {
@@ -20,13 +20,13 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
 
-        Label lbl = new Label("Hello");
-        Button btn = new Button("Click");
+        Label label1 = new Label("Label1");
+        Label label2 = new Label("Label2");
 
-        FlowPane root = new FlowPane();
-        root.getChildren().addAll(lbl, btn);
+        FlowPane root = new FlowPane(10, 10, label1, label2);
+        root.setAlignment(Pos.CENTER);
         Scene scene = new Scene(root, 300, 200);
         stage.setScene(scene);
 

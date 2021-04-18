6f60a2c6f3682a2702ec1c1b9d66284f1fc0db0f
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-18 18:59:16 +0300

    Application klass ja rakenduse elutsükkel

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -2,28 +2,29 @@ package ee.khk;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.text.Text;
+
 
 public class Main extends Application {
     public static void main(String[] args) {
+        System.out.println("Launching Application");
         Application.launch(args);
     }
 
+
+    @Override
+    public void init() throws Exception {
+        System.out.println("Application inits");
+        super.init();
+    }
+    @Override
     public void start(Stage stage) {
-        Text text = new Text("Tere Java FX!");
-        text.setLayoutY(80);
-        text.setLayoutX(100);
-
-        Group group = new Group(text);
-
-        Scene scene = new Scene(group);
-
-        stage.setScene(scene);
-        stage.setTitle("Esimene JavaFX rakendus");
-        stage.setWidth(300);
-        stage.setHeight(250);
+        System.out.println("Application starts");
         stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("Application stops");
+        super.stop();
     }
 }

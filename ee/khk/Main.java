1ab0bfbe26243eba1f7d5bd679322dc5d1614c15
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-19 20:25:05 +0300

    ListView

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,11 +1,11 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Orientation;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -21,25 +21,18 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
 
-        Label lbl = new Label("Value");
+        ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
+        ListView<String> langsListView = new ListView<String>(langs);
+        langsListView.setPrefSize(250, 150);
 
-        Slider slider = new Slider(0.0,20.0,10.0);
-        slider.setOrientation(Orientation.VERTICAL);
+        Button btn = new Button("Change");
 
-        slider.setShowTickMarks(true);
-        slider.setShowTickLabels(true);
-        slider.setBlockIncrement(2.0);
-        slider.setMajorTickUnit(5.0);
-        slider.setMinorTickCount(4);
-        slider.setSnapToTicks(true);
-        slider.valueProperty().addListener(new ChangeListener<Number>() {
-
-            public void changed(ObservableValue<? extends Number> changed, Number oldValue, Number newValue){
-                lbl.setText("Slider Value: " + newValue);
-            }
+        btn.setOnAction(event -> {
+            ObservableList<String> newLangs = FXCollections.observableArrayList("PHP", "Go", "C++");
+            langsListView.setItems(newLangs);
         });
 
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, slider, lbl);
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, btn, langsListView);
         Scene scene = new Scene(root, 300, 250);
 
         stage.setScene(scene);

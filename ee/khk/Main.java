840a163d3206bab946a63278b019d2856e030d0a
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-19 20:33:16 +0300

    ChoiceBox

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Orientation;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -22,16 +22,16 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception{
 
         ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
-        ComboBox<String> langsComboBox = new ComboBox<String>(langs);
-        langsComboBox.setValue("Java"); // määrame vaikimisi valitud element
+        ChoiceBox<String> langsChoiceBox = new ChoiceBox<String>(langs);
+        langsChoiceBox.setValue("Java");
 
         Label lbl = new Label();
-
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10,  langsComboBox, lbl);
+        langsChoiceBox.setOnAction(event -> lbl.setText(langsChoiceBox.getValue()));
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10,  langsChoiceBox, lbl);
         Scene scene = new Scene(root, 300, 250);
 
         stage.setScene(scene);
-        stage.setTitle("ComboBox in JavaFX");
+        stage.setTitle("ChoiceBox in JavaFX");
         stage.show();
     }
 }

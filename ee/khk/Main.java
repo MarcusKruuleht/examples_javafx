7d526697a9de646b3bea94c87f2f1e358571e1e3
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-19 19:58:28 +0300

    Tekstilised juhtimiselemendid

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,12 +1,15 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
-import javafx.scene.control.*;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+
 
 public class Main extends Application {
 
@@ -18,33 +21,18 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
 
-        Label selectedLbl = new Label();
-        Button selectBtn = new Button("Select");
-
-        RadioButton javaBtn = new RadioButton("Java");
-        RadioButton jsBtn = new RadioButton("JavaScript");
-        RadioButton csharpBtn = new RadioButton("C#");
-
-        ToggleGroup group = new ToggleGroup();
-
-        javaBtn.setToggleGroup(group);
-        jsBtn.setToggleGroup(group);
-        csharpBtn.setToggleGroup(group);
-
-        selectBtn.setOnAction(event -> {
-            RadioButton selection = (RadioButton) group.getSelectedToggle();
-            selectedLbl.setText("Selected " + selection.getText());
-        });
-
-        FlowPane root = new FlowPane(Orientation.VERTICAL,10, 10);
-        root.getChildren().addAll(javaBtn, jsBtn, csharpBtn, selectBtn, selectedLbl);
-        root.setPadding(new Insets(10));
-        Scene scene = new Scene(root, 250, 200);
+        Label lbl = new Label();
+        TextArea textArea = new TextArea();
+        textArea.setPrefColumnCount(15);
+        textArea.setPrefRowCount(5);
+        Button btn = new Button("Click");
+        btn.setOnAction(event -> lbl.setText("Input " + textArea.getText()));
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, textArea, btn, lbl);
+        root.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(root, 300, 250);
 
         stage.setScene(scene);
-
         stage.setTitle("Hello JavaFX");
-
         stage.show();
     }
 }

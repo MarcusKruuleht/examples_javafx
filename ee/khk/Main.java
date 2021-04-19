577d1657dda4614aeaa9c0a367d8ba0634ee2f41
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-19 18:11:00 +0300

    BorderPane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,15 +1,9 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -25,30 +19,19 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Button okBtn = new Button("OK");
-        Button cancelBtn = new Button("Cancel");
-        Label lbl = new Label("Select");
+        BorderPane root = new BorderPane();
 
-        VBox vbox = new VBox(15);
+        Button top = new Button("Top");
+        BorderPane.setAlignment(top, Pos.CENTER);
+        root.setTop(top);
 
-        VBox.setVgrow(okBtn, Priority.ALWAYS);
-        okBtn.setMaxHeight(Double.MAX_VALUE);
-        okBtn.setMaxWidth(100);
-        // VBox täislaius
-        // okBtn.setMaxWidth(Double.MAX_VALUE);
+        Button center = new Button("Center");
+        root.setCenter(center);
 
-        VBox.setVgrow(cancelBtn, Priority.ALWAYS);
-        cancelBtn.setMaxHeight(Double.MAX_VALUE);
-        cancelBtn.setMaxWidth(100);
-        // VBox täislaius
-        // cancelBtn.setMaxWidth(Double.MAX_VALUE);
-
-        vbox.getChildren().addAll(lbl, okBtn, cancelBtn);
-
-        Scene scene = new Scene(vbox, 300, 150);
+        Scene scene = new Scene(root, 300, 150);
         stage.setScene(scene);
 
-        stage.setTitle("VBox in JavaFX");
+        stage.setTitle("BorderPane in JavaFX");
 
         stage.show();
     }

a9217038df1e993e456bcc69943b2adab6400651
Marcus Kruuleht <marcus.kruuleht@khk.ee>
2021-04-19 19:03:35 +0300

    Nupud ja sildid

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,20 +1,20 @@
 package ee.khk;
 
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.VPos;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.RowConstraints;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 
 
 public class Main extends Application {
+
+    int clicks = 0;
 
     public static void main(String[] args) {
 
@@ -24,60 +24,27 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        Button first = new Button("First");
-        // paigutame nupp paremal alumisel nurgal
-        GridPane.setHalignment(first, HPos.RIGHT);
-        GridPane.setValignment(first, VPos.BOTTOM);
+        Label lbl = new Label("Counter");
+        lbl.setPrefWidth(70);
+        Button btn = new Button("Click");
+        btn.setPrefWidth(80);
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
 
-        Button second = new Button("Second");
-        // venitame nup horisontaalselt
-        second.setMaxWidth(Double.MAX_VALUE);
-        GridPane.setHgrow(second, Priority.ALWAYS);
+                clicks++;
+                lbl.setText(String.valueOf(clicks));
+            }
+        });
 
-        Button third = new Button("Third");
-        // venitame nupp horisontaalselt ja vertikaalselt
-        third.setMaxWidth(Double.MAX_VALUE);
-        third.setMaxHeight(Double.MAX_VALUE);
-        GridPane.setHgrow(third, Priority.ALWAYS);
-        GridPane.setVgrow(third, Priority.ALWAYS);
+        FlowPane root = new FlowPane(lbl, btn);
+        Scene scene = new Scene(root);
 
-        Button fourth = new Button("Fourth");
-        // nupp on sama suur nagu tabeli pesa
-        fourth.setMaxWidth(Double.MAX_VALUE);
-        fourth.setMaxHeight(Double.MAX_VALUE);
-        GridPane.setHgrow(fourth, Priority.ALWAYS);
-        GridPane.setVgrow(fourth, Priority.ALWAYS);
-        // määrame taane 10 pixlit
-        GridPane.setMargin(fourth, new Insets(10));
-
-        GridPane root = new GridPane();
-
-        ColumnConstraints column1 = new ColumnConstraints();
-        column1.setPercentWidth(50);
-        root.getColumnConstraints().add(column1);
-
-        ColumnConstraints column2 = new ColumnConstraints();
-        column2.setPercentWidth(50);
-        root.getColumnConstraints().add(column2);
-
-        RowConstraints row1 = new RowConstraints();
-        row1.setPercentHeight(50);
-        root.getRowConstraints().add(row1);
-
-        RowConstraints row2 = new RowConstraints();
-        row2.setPercentHeight(50);
-        root.getRowConstraints().add(row2);
-
-        root.setGridLinesVisible(true);
-        root.add(first,0,0);
-        root.add(second,0,1);
-        root.add(third,1,0);
-        root.add(fourth,1,1);
-
-        Scene scene = new Scene(root, 300, 200);
         stage.setScene(scene);
 
-        stage.setTitle("GridPane in JavaFX");
+        stage.setTitle("Hello JavaFX");
+        stage.setWidth(250);
+        stage.setHeight(200);
 
         stage.show();
     }
